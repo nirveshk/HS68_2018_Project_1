2b169c76dbc8dca2277ae1fd2c5df99b78ac3ec8
Nirvesh <nirveshk@gmail.com>
2018-07-21 13:30:43 -0700

    New sets of codes added- further work is still required to accomplish the issue.
    Also adding temporary heart.csv file, planning to delete it in later commit when test code is complete.

--- hs628_tools/Test.py
+++ hs628_tools/Test.py
@@ -1,11 +1,54 @@
 
 import numpy as np
+from sklearn import datasets
+import csv
+import os
 
-def test(dataset):
-    dataset = np.genfromtxt('heart.csv', delimiter =',', skip_header = 1)
 
-    for i in range(len(dataset)):
-        if dataset[i][:] == 'NA':
-            print 'what'
 
-test(dataset)
+
+class data_split:
+    """
+    Create class data_split to predict a perfect train and test data split ratio
+
+    define methods to accomplish this task
+
+    return a split ratio
+
+    """
+    def __init__(self, data):
+        self.data = data
+
+    #set path to read the csv file in the directory
+    def test_train():
+        pathName = os.getcwd()
+
+        dataset = []
+        fileNames = os.listdir(pathName)
+
+        # read file with extension .csv
+        for fileNames in fileNames:
+            if fileNames.endswith(".csv"):
+                dataset.append(fileNames)
+
+        for point in dataset:
+            file = open(os.path.join(pathName, point), "rU")
+            data = csv.reader(file, delimiter=',')
+            #data_len =sum(1 for line in data)
+
+
+            for column in data:
+                for row in column:
+                    if row == "?":
+                        pass
+                    elif row == "NA":
+                        pass
+                    elif row == "NAN":
+                        pass
+                    else:
+                        print row
+
+
+
+
+    test_train()
