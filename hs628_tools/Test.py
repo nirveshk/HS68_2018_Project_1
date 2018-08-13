7976d81e42e9d0558c788bd03e4fbac788db339d
Nirvesh <nirveshk@gmail.com>
2018-08-12 17:19:22 -0700

    Final tuning to the codes, added if __name__ ==__main__, project completed

--- hs628_tools/Test.py
+++ hs628_tools/Test.py
@@ -10,104 +10,75 @@ class DataSplit:
 
     returns a split ratio.
 
-    Identifies Missing values as Nan for any missing value, return train and test dataframe with uniform division
+    Identifies Missing values as Nan for any missing value
+
+    Returns:  train and test dataframe with uniform division
     of NAN between train and test data per the data split ratio.
 
     """
 
-    def __init__(self, data=None):
-        self.data = data
 
-
-    def test_train():
+    def test_train(self):
         """
-        Reads in csv file using np.genfromtxt, counts number of NANs in the dataset
-        Pass in .csv file
+
+        Suggests train and test ratio
+
+        set calculation standard for specific datalengths, this is based on the general rule of thumb
+        ratio increases as the length of the data increases, so to ensure we have  enough data to train and test
+        for the smaller dataset
+
+        prints out the train_percentage for user's convenience
+
+        Returns: new train and test set with uniform distribution of NAN values as tuple
 
         """
-        data = "heart.csv"
-        #'heart csv' can be downloaded from : https://github.com/nirveshk/HS68_2018_Project_1/tree/testtrain/hs628_tools
-        datafile = np.genfromtxt(data, delimiter=',', skip_header=1)
-        np.isnan(datafile).sum()  # counts the number of "NANs" in dat
 
-        print datafile.shape
-
-        # set calculation standard for specific datalengths, this is based on the general rule of thumb
-        # ratio increases as the length of the data increases, so to ensure we have  enough data to train and test
-        # for the smaller datasets
         row_len = len(datafile)
         if len(datafile) <= 500:
-            train = 60
-            test = 40
+            train_percent = 60
         elif len(datafile) > 500 and row_len <= 10000:
-            train = 70
-            test = 30
+            train_percent = 70
         elif len(datafile) > 10000 and row_len <= 100000:
-            train = 75
-            test = 25
+            train_percent = 75
         else:
-            train = 80
-            test = 20
-        print 'train:', train
+            train_percent = 80
+        print 'Train percentage for this data set:', train_percent
 
-        # drops all NAN values from data
+        # yields rows that do not contain nan data. This is done to accumulate nan data in
+        #seperate array and later, use the array to uniformly distribute among test and train data
         non_nandata = datafile[~np.isnan(datafile).any(axis=1)]
 
-        print non_nandata.shape
 
-        # print non_nandata.shape
-
-        ### nan dataframe
+        ### array that contains rows with corresponding NAN values- to be used later to plug in
+        #test and train set unifromly
         nan_data = datafile[np.isnan(datafile).any(axis=1)]
-        # print nan_data
 
-        #####split  Nan data and non_nandata using the split ratio
+        ##split  Nan data and non_nandata using the predeterminded split ratio
+        train_nan_data = nan_data[:int(train_percent* len(nan_data)/100), :]
+        test_nan_data = nan_data[int(train_percent* len(nan_data)/100):, :]
 
-        #####determined above
-
-        int_train = (train / float(100))
-        # print int_train
-
-        ##split Nan data to train and test sets
-
-        train_nan_data = nan_data[:int(train / float(100) * len(nan_data)), :]
-        test_nan_data = nan_data[int(train / float(100) * len(nan_data)):, :]
-
-        # print train_nan_data.shape
-
-        # print test_nan_data.shape
 
         ##split non Nan data to train and test sets
-        train_non_nan = non_nandata[:int(train / float(100) * len(non_nandata)), :]
-        test_non_nan = non_nandata[int(train / float(100) * len(non_nandata)):, :]
+        train_non_nan = non_nandata[:int(train_percent * len(non_nandata)/100), :]
+        test_non_nan = non_nandata[int(train_percent * len(non_nandata)/100):, :]
 
-        # print train_non_nan.shape
-
-        # print test_non_nan.shape
-
-        ####Add train_non_nan and train_nan_data
-
+        #Add train_non_nan and train_nan_data to create a final train data array
         final_train = np.concatenate([train_non_nan, train_nan_data])
-        # print final_train.shape
 
-        #### Add test_non_nan and test_nan_data
-
+        #### sum of  test_non_nan and test_nan_data
         final_test = np.concatenate([test_non_nan, test_nan_data])
 
-        # print final_test.shape
-
-        ##This module prints out the train and test data as per the preset ratio
-        ## determined based on the length of the data and includes missing data
-        ## uniformly between train and test data
-
-        final_train = "This is the final train data:" \
-            , final_train
-        final_test = "This is the final test data:" \
-            , final_test
-        return final_train, final_test
+        return "new_train_data :", final_train,\
+               "new_test_data:", final_test
 
 
-    test_train()
+if __name__ == '__main__':
+    data = "heart.csv"
+    #'heart csv' can be downloaded from : http://www.cs.usfca.edu/~pfrancislyon/data/heart.csv
+    datafile = np.genfromtxt(data, delimiter=',', skip_header=1)
+    np.isnan(datafile).sum()  # counts the number of "NANs" in data
+    new_test_train = DataSplit().test_train()
+    print new_test_train
 
 
 
